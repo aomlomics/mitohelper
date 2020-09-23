8941b396e4bcc67338a3b4216d32f2bc91d2d75e
shenjean <44844869+shenjean@users.noreply.github.com>
2020-09-23 18:58:57 -0400

    Flexible blastn and better graphs

--- mitohelper.py
+++ mitohelper.py
@@ -7,6 +7,7 @@ import os
 from os import path
 
 import matplotlib.pyplot as plt
+import matplotlib.patches as mpatches
 import seaborn as sns
 import pandas as pd
 
@@ -112,12 +113,12 @@ def getrecord(input_file,output_prefix,database_file,tax_level,fasta):
 
 
 @mitohelper.command()
-@click.option('-i','--input_file', required=True, type=str, help='Input file: either blast output file or FASTA file')
+@click.option('-i','--input_file', required=True, type=str, help='Input file: either blastn output file or FASTA file')
 @click.option('-o','--output_prefix', required=True, type=str, help='Output prefix (e.g. OUT)')
 @click.option('-r','--reference_sequence', required=False, type=str, help='FASTA file of a single reference sequence for blastn searches. Required for --blast option.')
-@click.option('--blast/--no-blast', default=False, help='Perform local blastn-short searches to extract alignment positions (default=FALSE)')
+@click.option('--blastn-task', type=click.Choice(['blastn','blastn-short','megablast','dc-megablast','none']), required=True, default='none', help='Choice of blastn task for local similarity searches used to extract alignment positions [default:blastn-short]')
 
-def getalignment(input_file,output_prefix,reference_sequence,blast):
+def getalignment(input_file,output_prefix,reference_sequence,blastn_task):
 	
 	"""Pairwise align input sequences against a reference"""
 	
@@ -134,7 +135,7 @@ def getalignment(input_file,output_prefix,reference_sequence,blast):
 	def parse_blast(results):
 		hits=tuple(open(results,'r'))
 		outfile=open(tsv_out,'a')
-		outfile.write("Accession\tStart\tEnd\n")
+		outfile.write("Accession\tStart\tEnd\tBit_Score\n")
 		i = 0
 		seen=set()
 
@@ -160,7 +161,7 @@ def getalignment(input_file,output_prefix,reference_sequence,blast):
 
 					if (i>0):
 						if (query is not None):
-							outfile.write("%s\t%s\t%s\n" % (query,startmin,endmax))
+							outfile.write("%s\t%d\t%d\t%d\n" % (query,startmin,endmax,score))
 					if (i>=0):
 						seen.add(acc)	
 						query=acc
@@ -177,12 +178,12 @@ def getalignment(input_file,output_prefix,reference_sequence,blast):
 				
 
 			if (i==(len(hits)-1)):
-				outfile.write("%s\t%s\t%s\n" % (query,startmin,endmax))
+				outfile.write("%s\t%d\t%d\t%d\n" % (query,startmin,endmax,lastscore))
 
 		outfile.close()
 
 
-	if blast:
+	if (blastn_task!='none'):
 		blast_out=(output_prefix+".blastn.txt")
 		blast_query=(output_prefix+".blastquery.fasta")
 		if path.exists(blast_out):
@@ -199,28 +200,42 @@ def getalignment(input_file,output_prefix,reference_sequence,blast):
         			with open(fname) as infasta:
             				merge.write(infasta.read())
 
-		blastn=subprocess.Popen(['blastn', '-task', 'blastn-short', '-query', blast_query, '-subject', reference_sequence, '-outfmt', '7', '-out', blast_out])
+		blastn=subprocess.Popen(['blastn', '-task', blastn_task, '-query', blast_query, '-subject', reference_sequence, '-outfmt', '7', '-out', blast_out])
 		blastn.communicate()
 		parse_blast(blast_out)
 		os.remove(blast_query)
 
-	if not blast:
+	if (blastn_task=='none'):
 		parse_blast(input_file)
 
 	positions=pd.read_table(tsv_out)
-	positions=pd.melt(positions,"Accession",var_name="PositionType")
-	positions.columns=['Accession','PositionType','Position']
+	positions=pd.melt(positions,['Accession','Bit_Score'],var_name='PositionType')
+	positions.columns=['Accession','Bit_Score','PositionType','Position']
 
 	start = positions[positions["PositionType"] == "Start"]
 	end = positions[positions["PositionType"] == "End"]
+	vmax = float(max(positions['Bit_Score']))
+        
+	if (vmax<200):
+		vmax=500
+
+	# Place bit scores into categories
+	cut_cat=['<40','40-50','50-80','80-200','>=200']
+	cut_bins =[0, 40, 50, 80, 200, vmax]
+	cpalette ={0: 'k', 1: 'b', 2: 'lime', 3: 'm', 4: 'r'}
+
+	positions['Score Category'] = pd.cut(positions['Bit_Score'], bins=cut_bins, labels = cut_cat)
+	ncat=positions['Score Category'].nunique()
+	positions['Bit Score Range'] = positions['Score Category'].cat.codes
 	
 	sns.set(style="white")
 
-	posplot=sns.pointplot(x="Position", y="Accession", hue="Accession", markers="", data=positions)
-	posplot.legend_.remove()
+	posplot=sns.pointplot(x='Position', y='Accession', hue='Bit Score Range', palette=cpalette, join=False, markers="", data=positions)
+
 	plt.title("Sequence alignment(s) relative to reference", size=14)
 	plt.grid(linestyle=":")
 
+	# Plot alignment positions on graph
 	for xmin,ymin in enumerate(start['Position']):
   		if(ymin==1):
     			plt.text(-30,xmin,ymin,fontsize=8,bbox=dict(facecolor='lightgrey', alpha=0.5))
@@ -229,12 +244,20 @@ def getalignment(input_file,output_prefix,reference_sequence,blast):
 
 	for xmax,ymax in enumerate(end['Position']):
     		plt.text(ymax+30,xmax,ymax,fontsize=8,bbox=dict(facecolor='lightgrey', alpha=0.5))
+
+	# Make customized legend
+	red = mpatches.Patch(color='red')
+	magenta = mpatches.Patch(color='m')
+	lime = mpatches.Patch(color='lime')
+	blue = mpatches.Patch(color='b')
+	black = mpatches.Patch(color='k')
+	plt.legend((red, magenta, lime, blue, black), ('>=200', '80-200', '50-80', '40-50', '<40'), loc='upper right', bbox_to_anchor=(1.4,1), title='Bit Scores')
 	
 	plt.savefig(pdf_out,bbox_inches='tight')
 
 	print ("==== Run complete! ===")
 
-	if blast:
+	if (blastn_task!='none'):
 		print ("blastn output saved in %s" % blast_out)
 
 	print ("Table of alignment positions saved in %s" % tsv_out)
