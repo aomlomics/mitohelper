264ccd404c89f9af12ae4a3e40eb74ed46845724
shenjean <44844869+shenjean@users.noreply.github.com>
2021-03-08 23:04:49 -0500

    Automated script to generate 12S and COI datasets

---
+++ db.scripts/make_ref.sh
@@ -0,0 +1,36 @@
+#!/usr/bin/env bash
+
+month=Mar2021
+
+# Search for 12S genes and complete mitogenomes from main mitohelper reference file
+
+grep -i -e "12S ribo" -e "12S rRNA" -e "12S small" \
+-e "complete genome" -e "complete mito"  \
+mitofish.all."$month".tsv >mitofish.12S.partI.tsv
+
+# Extract 12S genes from other studies with NCBI records that may not contain the keywords above 
+
+sed "s/^/\^/" 12S.unconventional.acc >12S.unconventional.grep 
+
+for i in `cat 12S.unconventional.grep`
+do
+grep -m 1 $i mitofish.new.tsv >>mitofish.12S.unconventional.tsv
+done
+
+# Combine 12S datasets and de-duplicate
+
+cat mitofish.12S.partI.tsv mitofish.12S.unconventional.tsv | sort | uniq >mitofish.12S.noheader.tsv
+cat mitofish.header mitofish.12S.noheader.tsv >mitofish.12S."$month".tsv
+
+# Extract COI hits and filter out cytochrome b records
+
+grep -i -e "(COI)" -e "CO1" -e "cytochrome c oxidase subunit I " \
+-e "cytochrome c oxidase subunit 1" -e "COI " -e "complete genome" -e "complete mito" \
+-e "COX1" -e "(COXI)" -e "cytochrome oxidase subunit I " mitofish.all."$month".tsv \
+| grep -v -i "cytochrome b" | grep -v -i "cytb"  >mitofish.COI.partI.tsv
+
+# Extract COI records that may not contain the keywords above 
+grep -f COI.unconventional.acc mitofish.all."$month".tsv >mitofish.COI.unconventional.tsv
+
+# Combine COI datasets and de-duplicate
+cat mitofish.header mitofish.COI.partI.tsv mitofish.COI.unconventional.tsv >mitofish.COI."$month".tsv
