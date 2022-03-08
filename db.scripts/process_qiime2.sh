4b87b2f5a2000fce2eb235a2595eb191be805f42
shenjean <44844869+shenjean@users.noreply.github.com>
2022-03-07 23:05:21 -0500

    echo to show status

--- db.scripts/process_qiime2.sh
+++ db.scripts/process_qiime2.sh
@@ -1,6 +1,6 @@
 #!/usr/bin/env bash
 
-month=Mar2021
+month=Mar2022
 
 # Download and export QIIME-compatible SILVA NR99 full-length sequences and taxonomy
 # wget https://data.qiime2.org/2020.11/common/silva-138-99-seqs.qza
@@ -8,45 +8,46 @@ month=Mar2021
 # qiime tools export --input-path silva-138-99-seqs.qza --output-path silva138.99.seq
 # qiime tools export --input-path silva-138-99-tax.qza --output-path silva138.99.tax
 
-# Convert mitofish 12S table into FASTA file
+echo "Convert mitofish 12S table into FASTA file.."
 cat mitofish.12S."$month".tsv | awk -F "\t" '{OFS="#"}{print $1,$11}' | grep -v "Accession#Sequence" | sed "s/^/>/" | tr "#" "\n" >mitofish.12S."$month".fasta
 
-# Extract list of accession numbers (feature IDs)
+echo "Extract list of accession numbers (feature IDs).."
 cut -f1 mitofish.12S."$month".tsv | sed "s/Accession/Feature ID/" >12S.featureID
 
-# Extract taxonomic information (domain through species)
+echo "Extract taxonomic information (domain through species).."
 cut -f6 mitofish.12S."$month".tsv | sed "s/^/d__Eukaryota; p__Chordata; c__/" | sed "s/$/;/" >12S.class
 cut -f7 mitofish.12S."$month".tsv | sed "s/^/o__/" | sed "s/$/;/" >12S.order
 cut -f8 mitofish.12S."$month".tsv | sed "s/^/f__/" | sed "s/$/;/" >12S.family
 cut -f9 mitofish.12S."$month".tsv | sed "s/^/g__/" | sed "s/$/;/" >12S.genus
 cut -f10 mitofish.12S."$month".tsv | sed "s/^/s__/" >12S.species
 
-# Combine all taxonomic name columns
+echo "Combine all taxonomic name columns.."
 paste -d " " 12S.class 12S.order 12S.family 12S.genus 12S.species | sed "s/.*f__Family.*$/Taxon/" >12S.taxon
 
-# Merge feature ID and taxon information
+echo "Merge feature ID and taxon information.."
 paste -d "\t" 12S.featureID 12S.taxon >12S.taxonomy.tsv
 
-# Import 12S sequences and taxonomies into QIIME2
+echo "Import 12S sequences and taxonomies into QIIME2.."
 qiime tools import --type 'FeatureData[Sequence]' --input-path mitofish.12S."$month".fasta --output-path 12S-seqs.qza
 qiime tools import --type 'FeatureData[Taxonomy]' --input-path 12S.taxonomy.tsv --output-path 12S-tax.qza
 
-# Remove sequences with >=5 ambiguous bases and homopolymers>=8bp long
+echo "Remove sequences with >=5 ambiguous bases and homopolymers>=8bp long.."
 qiime rescript cull-seqs --i-sequences 12S-seqs.qza --o-clean-sequences 12S-seqs-cleaned.qza
 
-# Dereplicate cleaned sequences in uniq mode to retain identical sequence records with differing taxonomies
+echo "Dereplicate cleaned sequences in uniq mode to retain identical sequence records with differing taxonomies.."
+# Use vsearch bundled with qiime2 environment (V2.7.0). Do not use latest version of vsearch in bash profile
 qiime rescript dereplicate --i-sequences 12S-seqs-cleaned.qza --i-taxa 12S-tax.qza --p-rank-handles 'silva' --p-mode 'uniq' --o-dereplicated-sequences 12S-seqs-derep-uniq.qza --o-dereplicated-taxa 12S-tax-derep-uniq.qza
 
-# Export cleaned and dereplicated 12S data
+echo "Export cleaned and dereplicated 12S data.."
 qiime tools export --input-path 12S-seqs-derep-uniq.qza --output-path 12S.seq
 qiime tools export --input-path 12S-tax-derep-uniq.qza --output-path 12S.tax
 
-# Concatenate 12S+SILVA (16S & 18S) data:
+echo "Concatenate 12S+SILVA (16S & 18S) data.."
 head -1 12S.tax/taxonomy.tsv >tax.header
 cat 12S.tax/taxonomy.tsv silva138.99.tax/taxonomy.tsv | grep -v "Feature ID" >SSU.taxonomy
 cat tax.header SSU.taxonomy >SSU.taxonomy.tsv
 cat 12S.seq/dna-sequences.fasta silva138.99.seq/dna-sequences.fasta >SSU.fasta
 
-# Import 12S+SILVA (16S & 18S) data into QIIME 2
+echo "Import 12S+SILVA (16S & 18S) data into QIIME 2.."
 qiime tools import --type 'FeatureData[Sequence]' --input-path SSU.fasta --output-path 12S-16S-18S-seqs.qza
 qiime tools import --type 'FeatureData[Taxonomy]' --input-path SSU.taxonomy.tsv --output-path 12S-16S-18S-tax.qza
