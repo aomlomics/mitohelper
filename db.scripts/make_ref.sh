21ea2df3e5746d4f9e9172fd64107b0c737b9efb
shenjean <44844869+shenjean@users.noreply.github.com>
2022-03-07 23:04:45 -0500

    echo to show status

--- db.scripts/make_ref.sh
+++ db.scripts/make_ref.sh
@@ -1,8 +1,10 @@
 #!/usr/bin/env bash
 
-month=Jul2021
+month=Mar2022
 
 # Search for 12S genes and complete mitogenomes from main mitohelper reference file
+
+echo "Processing 12S..."
 
 grep -i -e "12S ribo" -e "12S rRNA" -e "12S small" \
 -e "complete genome" -e "complete mito"  \
@@ -10,12 +12,12 @@ mitofish.all."$month".tsv >mitofish.12S.partI.tsv
 
 # Extract 12S genes from other studies with NCBI records that may not contain the keywords above 
 
-sed "s/^/\^/" 12S.unconventional.acc >12S.unconventional.grep 
+#sed "s/^/\^/" 12S.unconventional.acc >12S.unconventional.grep 
 
-for i in `cat 12S.unconventional.grep`
-do
-grep -m 1 $i mitofish.all."$month".tsv >>mitofish.12S.unconventional.tsv
-done
+#for i in `cat 12S.unconventional.grep`
+#do
+#grep -m 1 $i mitofish.all."$month".tsv >>mitofish.12S.unconventional.tsv
+#done
 
 # Combine 12S datasets and de-duplicate
 
@@ -24,13 +26,15 @@ cat mitofish.header mitofish.12S.noheader.tsv >mitofish.12S."$month".tsv
 
 # Extract COI hits and filter out cytochrome b records
 
+echo "Processing COI..."
+
 grep -i -e "(COI)" -e "CO1" -e "cytochrome c oxidase subunit I " \
 -e "cytochrome c oxidase subunit 1" -e "COI " -e "complete genome" -e "complete mito" \
 -e "COX1" -e "(COXI)" -e "cytochrome oxidase subunit I " mitofish.all."$month".tsv \
 | grep -v -i "cytochrome b" | grep -v -i "cytb"  >mitofish.COI.partI.tsv
 
 # Extract COI records that may not contain the keywords above 
-grep -f COI.unconventional.acc mitofish.all."$month".tsv >mitofish.COI.unconventional.tsv
+#grep -f COI.unconventional.acc mitofish.all."$month".tsv >mitofish.COI.unconventional.tsv
 
 # Combine COI datasets and de-duplicate
 cat mitofish.header mitofish.COI.partI.tsv mitofish.COI.unconventional.tsv >mitofish.COI."$month".tsv
